6b907ba58af20aed80a08e06a99c80d73e9589f5
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-06-26 22:04:14 +0200

    replace module with brand new crate

--- module/src/lib.rs
+++ module/src/lib.rs
@@ -1,42 +1,14 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::collections::HashMap;
-use std::error::Error;
-use std::fmt::Display;
-use thiserror::Error;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
-const DESCRIPTION: &str = "description";
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// A identifier string. Must be unique within it's context.
-#[derive(Debug, Eq, Hash, PartialEq, Clone, PartialOrd, Ord, Serialize, Deserialize)]
-pub struct Identifier(String);
-
-impl From<String> for Identifier {
-    fn from(value: String) -> Self {
-        Identifier(value)
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
     }
 }
-
-impl Display for Identifier {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-/// A generic object that contains data
-pub type JsonObject = HashMap<String, Value>;
-
-/// The errors that can happen when constructing a Module
-#[derive(Error, Debug)]
-pub enum ModuleError {
-    #[error("unable to build element")]
-    UnableToBuildElement(#[from] Box<dyn Error>),
-    #[error("received value is not an object")]
-    NotAnObject,
-    #[error("missing required field `{0}`")]
-    MissingRequiredField(String),
-}
-
-pub mod module;
-pub mod module_type;
-pub mod rendering;
--- module/src/rendering/helpers/split_lines.rs
+++
@@ -1,3 +0,0 @@
-use handlebars::handlebars_helper;
-
-handlebars_helper!(split_lines: |paragraph: String| paragraph.split('\n').collect::<Vec<&str>>());
