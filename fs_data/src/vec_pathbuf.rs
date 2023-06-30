ccc8235421f8d7f1a967bc9972a9d87c41d47350
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-06-30 09:43:01 +0200

    add tests to data from pathbuf

--- fs_data/src/vec_pathbuf.rs
+++ fs_data/src/vec_pathbuf.rs
@@ -7,7 +7,6 @@ use serde_json::Value;
 use crate::{EntryData, FileSystemDataError};
 
 impl EntryData for Vec<PathBuf> {
-    // TODO: implement tests
     fn try_get_data(&self) -> Result<Value, FileSystemDataError> {
         let mut result: HashMap<String, Value> = HashMap::new();
         for file in self {
@@ -20,5 +19,37 @@ impl EntryData for Vec<PathBuf> {
         }
         serde_json::to_value(result)
             .map_err(|e| FileSystemDataError::UnableToSerializeResult(e.into()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use path_utils::create_test_file;
+    use pretty_assertions::assert_eq;
+    use serde_json::json;
+    use testdir::testdir;
+
+    #[test]
+    fn each_path_becomes_an_entry() {
+        let dir = testdir!();
+
+        let file_a = create_test_file(&dir.join("a.txt"), "a");
+        let file_b = create_test_file(&dir.join("b.txt"), "b");
+        let file_c = create_test_file(&dir.join("c.txt"), "c");
+        let file_d = create_test_file(&dir.join("d.txt"), "d");
+
+        let vect = vec![file_a, file_b, file_c, file_d];
+
+        assert_eq!(
+            vect.try_get_data().unwrap(),
+            json!({
+                "a": "a",
+                "b": "b",
+                "c": "c",
+                "d": "d",
+            })
+        );
     }
 }
